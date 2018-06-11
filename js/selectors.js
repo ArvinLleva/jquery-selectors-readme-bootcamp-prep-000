783367d7028161842e5a734256471f07140b39f1
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-11 21:42:33 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -16,5 +16,9 @@ function divSelector(){
 }
 
 function firstListItem(){
+<<<<<<< HEAD
   return $('ul img:first-child').get(0)// the get function get's the first index from the listed items with those of ID "pic-list";
+=======
+  return $('ul #pic-list:first-child');
+>>>>>>> bbf1ace6bf2a88a2ce5bac575a26eac4df9d2be8
 }
